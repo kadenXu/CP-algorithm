6c04edb24cbe3cc5b2cb2f867e59a703ce6d573b
Yunhao Xu <61392789+kadenXu@users.noreply.github.com>
2021-07-11 14:13:40 -0400

    Update and rename generating-subset to generating-subset.cpp

--- generating-subset.cpp
+++ generating-subset.cpp
@@ -1,13 +1,13 @@
 //generaing subset by bit sequences
 for (int b = 0; b < (1<<n); b++) {
-		vector<int> subset;
-		for (int i = 0; i < n; i++) {
-			if (b&(1<<i)) subset.push_back(i);
-		}
-		cout << "{ "; // printing
-		for (auto j: subset) cout << j << " ";
-		cout << "}\n"; // printing
-	}	
+	vector<int> subset;
+	for (int i = 0; i < n; i++) {
+		if (b&(1<<i)) subset.push_back(i);
+	}
+	cout << "{ "; // printing
+	for (auto j: subset) cout << j << " ";
+	cout << "}\n"; // printing
+}	
   
   
   // generating subset recursively

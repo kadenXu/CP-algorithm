39048a78d905de657714754f9ffae55e3c75f693
Kaden Xu <kadenxu123789@gmail.com>
2025-03-27 17:31:36 -0400

    Fix the referencing before initialization in the constructor when instantiating with data provided

--- Python/src/data_structures.py
+++ Python/src/data_structures.py
@@ -3,10 +3,10 @@ import heapq
 
 class Heap:
     def __init__(self, data=None, *, key=lambda x: x, ascending=True, **kwargs):
-        self._heap = [(self._construct_key(x), x) for x in data] if data else []
-        heapq.heapify(self._heap)
         self._comparator = key
         self._ascending = ascending
+        self._heap = [(self._construct_key(x), x) for x in data] if data else []
+        heapq.heapify(self._heap)
 
     def _construct_key(self, element):
         return self._comparator(element) * (1 if self._ascending else -1)
@@ -20,7 +20,3 @@ class Heap:
 
     def __len__(self):
         return len(self._heap)
-
-
-
-
